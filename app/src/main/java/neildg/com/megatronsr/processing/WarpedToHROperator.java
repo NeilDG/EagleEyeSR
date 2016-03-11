4f9653647ac471599e0f36043a8946709b4975d4
Neil Del Gallego <neil.dg@pocketplaylab.com>
2016-03-11 14:17:10 +0800

    Changes in AND/OR operation.

--- app/src/main/java/neildg/com/megatronsr/processing/WarpedToHROperator.java
+++ app/src/main/java/neildg/com/megatronsr/processing/WarpedToHROperator.java
@@ -25,7 +25,7 @@ public class WarpedToHROperator {
 
     private Mat outputMat;
 
-    private Mat previousMaskMat = new Mat();
+    private Mat baseMaskMat = new Mat();
 
     public WarpedToHROperator(List<Mat> warpedMatrixList) {
         this.warpedMatrixList = warpedMatrixList;
@@ -33,13 +33,26 @@ public class WarpedToHROperator {
     }
 
     public void perform() {
+        ProgressDialogHandler.getInstance().showDialog("Transforming warped images to HR", "Warping base image");
+        Mat baseWarpMKat = this.warpedMatrixList.get(0);
+        Mat baseHRWarpMat = Mat.zeros(baseWarpMKat.rows() * ParameterConstants.SCALING_FACTOR, baseWarpMKat.cols() * ParameterConstants.SCALING_FACTOR, baseWarpMKat.type());
+        this.copyMatToHR(baseWarpMKat, baseHRWarpMat, 0, 0);
 
-        for(int i = 0; i < this.warpedMatrixList.size(); i++) {
+        this.baseMaskMat = new Mat(baseHRWarpMat.rows(), baseHRWarpMat.cols(), CvType.CV_8UC1);
+        baseHRWarpMat.convertTo(this.baseMaskMat, CvType.CV_8UC1);
+        Imgproc.cvtColor(this.baseMaskMat, this.baseMaskMat, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.threshold(this.baseMaskMat, this.baseMaskMat, 1, 1, Imgproc.THRESH_TRUNC);
+
+        baseHRWarpMat.copyTo(this.outputMat, this.baseMaskMat);
+        ImageWriter.getInstance().saveMatrixToImage(this.outputMat, "result_0");
+
+        for(int i = 1; i < this.warpedMatrixList.size(); i++) {
             ProgressDialogHandler.getInstance().showDialog("Transforming warped images to HR", "Warped image " + i + " pixel stretching");
 
             Mat warpedMat = this.warpedMatrixList.get(i);
             Mat hrWarpedMat =  Mat.zeros(warpedMat.rows() * ParameterConstants.SCALING_FACTOR, warpedMat.cols() * ParameterConstants.SCALING_FACTOR, warpedMat.type());
 
+            //Imgproc.resize(warpedMat, hrWarpedMat, hrWarpedMat.size(), ParameterConstants.SCALING_FACTOR, ParameterConstants.SCALING_FACTOR, Imgproc.INTER_NEAREST);
             this.copyMatToHR(warpedMat, hrWarpedMat, 0, 0);
 
             ImageWriter.getInstance().saveMatrixToImage(hrWarpedMat, "hrwarp_" + i);
@@ -50,21 +63,21 @@ public class WarpedToHROperator {
             Imgproc.cvtColor(maskHRMat, maskHRMat, Imgproc.COLOR_BGR2GRAY);
             Imgproc.threshold(maskHRMat, maskHRMat, 1, 1, Imgproc.THRESH_TRUNC);
 
-            //only replace values that have not been touched by previous masks
-            if(this.previousMaskMat.elemSize() != 0) {
-                Core.bitwise_xor(this.previousMaskMat, maskHRMat, maskHRMat);
-            }
+            //perform filtering of mask
+            Mat comparingMat = new Mat();
+            Core.bitwise_not(this.baseMaskMat, comparingMat);
+            Core.bitwise_and(comparingMat, maskHRMat, maskHRMat);
 
             hrWarpedMat.copyTo(this.outputMat, maskHRMat);
             ImageWriter.getInstance().saveMatrixToImage(this.outputMat, "result_" + i);
 
-            //copy the mask mat to a holder
-            maskHRMat.copyTo(this.previousMaskMat);
+            //perform OR operation to merge the mask mat with the base MAT
+            Core.bitwise_or(this.baseMaskMat, maskHRMat, this.baseMaskMat);
         }
 
-        Mat bilateralMat = new Mat();
-        Imgproc.bilateralFilter(this.outputMat,bilateralMat,7, 200, 200);
-        ImageWriter.getInstance().saveMatrixToImage(bilateralMat, "FINAL_RESULT");
+        //Mat bilateralMat = new Mat();
+        //Imgproc.bilateralFilter(this.outputMat,bilateralMat,7, 200, 200);
+        ImageWriter.getInstance().saveMatrixToImage(this.outputMat, "FINAL_RESULT");
         ProgressDialogHandler.getInstance().hideDialog();
     }
 
