025ad6b9e8ab6666eb5c77ee9a777a63c9de1906
Neil Del Gallego <neil.dg@pocketplaylab.com>
2016-05-05 16:37:47 +0800

    Updated image patch pool.

--- app/src/main/java/neildg/com/megatronsr/model/single/ImagePatchPool.java
+++ app/src/main/java/neildg/com/megatronsr/model/single/ImagePatchPool.java
@@ -1,5 +1,12 @@
 package neildg.com.megatronsr.model.single;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import neildg.com.megatronsr.model.AttributeHolder;
+import neildg.com.megatronsr.model.AttributeNames;
+
 /**
  * Created by neil.dg on 5/5/16.
  */
@@ -20,10 +27,24 @@ public class ImagePatchPool {
     }
 
     public static final int MAX_LOADED_PATCHES = 200;
-    
+
     private int loadedPatches = 0;
 
-    private ImagePatchPool() {
+    private List<HashMap<String, ImagePatch>> patchPyramidList = new ArrayList<HashMap<String, ImagePatch>>();
 
+    private ImagePatchPool() {
+        int pyramidDepth = (int) AttributeHolder.getSharedInstance().getValue(AttributeNames.MAX_PYRAMID_DEPTH_KEY, 0);
+
+        for(int i = 0; i < pyramidDepth; i++) {
+            HashMap<String, ImagePatch> patchTable = new HashMap<>();
+            this.patchPyramidList.add(patchTable);
+        }
+    }
+
+    public void addPatch(int pyramidDepth, int col, int row, String imageName, String imagePath) {
+        ImagePatch patch  = new ImagePatch(col, row, imagePath);
+
+        HashMap<String,ImagePatch> patchTable = this.patchPyramidList.get(0);
+        patchTable.put(imageName, patch);
     }
 }
