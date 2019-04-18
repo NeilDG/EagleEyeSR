bc28672ec2bef70a549ed4341ffdf39e672e7da8
NeilDG <NeilDG@github.com>
2019-04-18 18:32:20 +0800

    Added dynamic permissions.

--- app/src/main/java/neildg/com/eagleeyesr/MainActivity.java
+++ app/src/main/java/neildg/com/eagleeyesr/MainActivity.java
@@ -1,16 +1,17 @@
 package neildg.com.eagleeyesr;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.darsh.multipleimageselect.activities.AlbumSelectActivity;
@@ -18,7 +19,6 @@ import com.darsh.multipleimageselect.helpers.Constants;
 import com.darsh.multipleimageselect.models.Image;
 
 import org.opencv.android.*;
-import org.opencv.core.Mat;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -30,7 +30,6 @@ import neildg.com.eagleeyesr.io.FileImageWriter;
 import neildg.com.eagleeyesr.model.AttributeHolder;
 import neildg.com.eagleeyesr.platformtools.core_application.ApplicationCore;
 import neildg.com.eagleeyesr.io.BitmapURIRepository;
-import neildg.com.eagleeyesr.processing.jni_bridge.SuperResJNI;
 import neildg.com.eagleeyesr.ui.progress_dialog.ProgressDialogHandler;
 import neildg.com.eagleeyesr.ui.views.AboutScreen;
 import neildg.com.eagleeyesr.ui.views.InfoScreen;
@@ -42,6 +41,8 @@ public class MainActivity extends AppCompatActivity{
     private boolean hasCamera = true;
 
     private int REQUEST_PICTURE_EXTERNAL = 1;
+    private final int PERMISSION_WRITE_EXTERNAL_STORAGE = 2;
+    private final int PERMISSION_READ_EXTERNAL_STORAGE = 3;
 
     static {
         System.loadLibrary("opencv_java3");
@@ -67,6 +68,9 @@ public class MainActivity extends AppCompatActivity{
     private InfoScreen infoScreen;
     private AboutScreen aboutScreen;
 
+    //private boolean readGranted = false;
+    private boolean writeGranted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,6 +92,7 @@ public class MainActivity extends AppCompatActivity{
 
         this.verifyCamera();
         this.initializeButtons();
+        this.requestPermission();
     }
 
     @Override
@@ -102,10 +107,6 @@ public class MainActivity extends AppCompatActivity{
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
-
-        DirectoryStorage.getSharedInstance().createDirectory();
-        FileImageWriter.initialize(this);
-        FileImageReader.initialize(this);
     }
 
     @Override
@@ -120,6 +121,93 @@ public class MainActivity extends AppCompatActivity{
         FileImageReader.destroy();
         super.onDestroy();
     }
+
+    private void requestPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSION_WRITE_EXTERNAL_STORAGE);
+
+        }
+        else {
+            this.writeGranted = true;
+        }
+
+        /*if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PERMISSION_READ_EXTERNAL_STORAGE);
+        }
+        else {
+            this.readGranted = true;
+        }*/
+
+
+        if(this.writeGranted) {
+            // Permission has already been granted
+            DirectoryStorage.getSharedInstance().createDirectory();
+            FileImageWriter.initialize(this);
+            FileImageReader.initialize(this);
+
+            Button captureImageBtn = (Button) this.findViewById(R.id.capture_btn);
+            captureImageBtn.setEnabled(true);
+
+            Button pickImagesBtn = (Button) this.findViewById(R.id.select_image_btn);
+            pickImagesBtn.setEnabled(true);
+
+            Button grantPermissionBtn = (Button) this.findViewById(R.id.button_grant_permission);
+            grantPermissionBtn.setVisibility(Button.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    this.writeGranted = true;
+                }
+                break;
+            }
+        }
+
+        if(this.writeGranted) {
+            DirectoryStorage.getSharedInstance().createDirectory();
+            FileImageWriter.initialize(this);
+            FileImageReader.initialize(this);
+
+            Button captureImageBtn = (Button) this.findViewById(R.id.capture_btn);
+            captureImageBtn.setEnabled(true);
+
+            Button pickImagesBtn = (Button) this.findViewById(R.id.select_image_btn);
+            pickImagesBtn.setEnabled(true);
+
+            Button grantPermissionBtn = (Button) this.findViewById(R.id.button_grant_permission);
+            grantPermissionBtn.setVisibility(Button.INVISIBLE);
+        }
+        else {
+            Toast.makeText(this, "Eagle-Eye needs to read and write temporary images for processing to your storage.", Toast.LENGTH_LONG)
+                    .show();
+
+            Button captureImageBtn = (Button) this.findViewById(R.id.capture_btn);
+            captureImageBtn.setEnabled(false);
+
+            Button pickImagesBtn = (Button) this.findViewById(R.id.select_image_btn);
+            pickImagesBtn.setEnabled(false);
+
+            Button grantPermissionBtn = (Button) this.findViewById(R.id.button_grant_permission);
+            grantPermissionBtn.setVisibility(Button.VISIBLE);
+        }
+    }
+
 
     private void verifyCamera() {
         PackageManager packageManager = ApplicationCore.getInstance().getAppContext().getPackageManager();
@@ -137,6 +225,14 @@ public class MainActivity extends AppCompatActivity{
             public void onClick(View view) {
                 Intent cameraIntent = new Intent(MainActivity.this, NewCameraActivity.class);
                 startActivity(cameraIntent);
+            }
+        });
+
+        Button grantPermissionBtn = (Button) this.findViewById(R.id.button_grant_permission);
+        grantPermissionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.requestPermission();
             }
         });
 
--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,3 +1,3 @@
-APP_ABI := armeabi-v7a arm64-v8a x86
+APP_ABI := armeabi-v7a arm64-v8a
 APP_PLATFORM := android-17
 APP_STL := stlport_static
